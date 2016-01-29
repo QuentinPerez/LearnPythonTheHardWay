adfa5221146c71fd1a2a9a164fe736a39b30efc9
Quentin Perez <qperez@ocs.online.net>
2016-01-29 14:26:33 +0100

    ex7

---
+++ ex7/ex7.py
@@ -0,0 +1,11 @@
+
+
+print "." * 10  # lol
+
+print "toto" + "titi",  # with comma at the end
+print "tata" + "tati"
+
+print ""  # new blank line
+
+print "toto" + "titi"   # without comma
+print "tata" + "tati"
