c7e7a451501c29236195883529d16bb3782bab07
Quentin Perez <qperez@ocs.online.net>
2016-01-29 14:20:26 +0100

    Add ex5

---
+++ ex5/ex5.py
@@ -0,0 +1,11 @@
+string1 = 'This is a string'
+num = 34
+string2 = "A string again"
+bool1 = True
+float1 = 50.9
+
+print "%s" % string1
+print "%d" % num
+print "%s" % string2
+print "%r" % bool1
+print "%f" % float1
